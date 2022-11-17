d86935b6e0a827c042269ff3b35ec27c7cd01d73
Marco <posta.email3001@gmail.com>
2022-11-17 13:19:34 +0100

    bonus_completo

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ ed emetto un messaggio in console con il numero della cella cliccata.*/
 //richiama elementi html
 const contenitore = document.getElementById("container");
 const myButton = document.getElementById("button");
+const difficolta = document.getElementById("difficulty");
 
 //genera griglia
 myButton.addEventListener("click",
@@ -15,28 +16,72 @@ myButton.addEventListener("click",
         contenitore.innerHTML = "";
         console.clear();
 
-        //genera 100 elementi (div) nella griglia
-        for (let i = 1; i <= 100; i++) {
+        //DIFFICOLTA' EASY
+        if (difficolta.value === "easy") {
+            
+            //genera 100 elementi (div) nella griglia
+            for (let i = 1; i <= 100; i++) {
 
-            const div = document.createElement("div");
-            div.classList.add("box");
-            div.append(i);
-            contenitore.append(div);
+                const div = document.createElement("div");
+                div.classList.add("box");
+                div.classList.add("easy");
+                div.append(i);
+                contenitore.append(div);
 
-            //al click aggiungi classe che colora la cella e stampa n° cella in console
-            div.addEventListener("click",
+                //al click aggiungi classe che colora la cella e stampa n° cella in console
+                div.addEventListener("click",
 
-                function(){
-                div.classList.add("azure");
-                console.log(i);
-                }
+                    function(){
+                    div.classList.add("azure");
+                    console.log(i);
+                    }
 
-            );
+                );
+            }
+        } else if (difficolta.value === "medium") { //DIFFICOLTA' MEDIUM
 
+            //genera 100 elementi (div) nella griglia
+            for (let i = 1; i <= 81; i++) {
+
+                const div = document.createElement("div");
+                div.classList.add("box");
+                div.classList.add("medium");
+                div.append(i);
+                contenitore.append(div);
+
+                //al click aggiungi classe che colora la cella e stampa n° cella in console
+                div.addEventListener("click",
+
+                    function(){
+                    div.classList.add("azure");
+                    console.log(i);
+                    }
+
+                );
+            }
+        } else { //DIFFICOLTA' HARD
+
+            //genera 100 elementi (div) nella griglia
+            for (let i = 1; i <= 49; i++) {
+
+                const div = document.createElement("div");
+                div.classList.add("box");
+                div.classList.add("hard");
+                div.append(i);
+                contenitore.append(div);
+
+                //al click aggiungi classe che colora la cella e stampa n° cella in console
+                div.addEventListener("click",
+
+                    function(){
+                    div.classList.add("azure");
+                    console.log(i);
+                    }
+
+                );
+            }
         }
-
     }
-
 );
 
  
